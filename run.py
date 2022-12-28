07ee62dc882787b08cc6b2494e1fa00e60d9d70d
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2022-12-28 13:27:01 +0500

    Added license information

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
+#!/usr/bin/python3
+
+# Copyright (C) 2022 Micromine
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "Dmitriy Kuptsov"
+__copyright__ = "Copyright 2022, Micromine"
+__license__ = "GPL"
+__version__ = "0.0.1b"
+__maintainer__ = "Dmitriy Kuptsov"
+__email__ = "dkuptsov@micromine.com"
+__status__ = "development"
+
 from tkinter import Tk, Canvas, Frame, BOTH
 
 import gc
